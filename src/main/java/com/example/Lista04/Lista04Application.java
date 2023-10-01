e840afa62a5a99703708fe3fb966884356a18dec
Poliana Abadio <polianabadio@gmail.com>
2023-10-01 20:44:16 -0300

    primeiro commit

---
+++ src/main/java/com/example/Lista04/Lista04Application.java
@@ -0,0 +1,13 @@
+package com.example.Lista04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lista04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lista04Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Lista04/Lista04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lista04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lista04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
